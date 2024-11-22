cce310d7db589f0101cb31c8237868d5b281d472
Abdurraouf <a.k.sadi80@gmail.com>
2024-11-22 00:28:20 -0600

    update

--- server/server.js
+++ server/server.js
@@ -1,21 +1,18 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const routes = require("./controller");
-require("dotenv").config();
-const auth = require("./helper/auth");
-const errorHandler = require("./helper/error-handler");
-const db = require("./config/connection");
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const routes = require('./controller');
+require('dotenv').config();
+const auth = require('./helper/auth');
+const errorHandler = require('./helper/error-handler');
+const db = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const subdomain = 'JAMDOMAIN';
 
 // Middleware
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/build'))); // Serve static files from React app
 
 // Authentication middleware
 app.use(auth());
@@ -26,27 +23,17 @@ app.use(routes);
 // Error handling middleware should be last
 app.use(errorHandler);
 
-// if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+// Serve static React build files
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+// Catch-all route for React's client-side routing
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-// Connect to the database and start the server
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+// Connect to DB and run API
+db.once('open', () => {
+  console.log('Connected to DB');
 });
 
-// Setup localtunnel
-const localtunnel = require('localtunnel');
-(async () => {
-  const tunnel = await localtunnel({ 
-    subdomain: subdomain, 
-    port: PORT  // Use the defined PORT variable
-  });
-  console.log(`App available at: ${tunnel.url}`);
-})();
+module.exports = app;  // Export the app for Vercel
