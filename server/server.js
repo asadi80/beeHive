73719729beb07633593dae0b41590a472c507336
Abdurraouf <a.k.sadi80@gmail.com>
2024-11-21 22:02:45 -0600

    update

--- server/server.js
+++ server/server.js
@@ -30,9 +30,9 @@ app.use(errorHandler);
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.send("Server is runing");
+});
 
 // Connect to the database and start the server
 db.once("open", () => {
