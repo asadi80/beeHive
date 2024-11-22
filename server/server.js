9003f58d044766bcec0d037fbc99e5cd8a6c341f
Abdurraouf <a.k.sadi80@gmail.com>
2024-11-21 23:59:51 -0600

    update

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(routes);
 app.use(errorHandler);
 
 // if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
 app.get('*', (req, res) => {
