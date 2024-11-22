cfbf447d65debfef668a20216dc4eea672a53a6c
Abdurraouf <a.k.sadi80@gmail.com>
2024-11-21 21:56:58 -0600

    update

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const subdomain = 'JAMDOMAIN';
 app.use(cors());
 app.options("*", cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/build'))); // Serve static files from React app
+// app.use(express.static(path.join(__dirname, '../client/build'))); // Serve static files from React app
 
 // Authentication middleware
 app.use(auth());
@@ -30,9 +30,9 @@ app.use(errorHandler);
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
 
 // Connect to the database and start the server
 db.once("open", () => {
